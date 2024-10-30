e31fb2f942eb6dd8ffd0211b2b6d437bca2a2301
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2024-10-31 00:08:42 +0700

    feat: add list coin list screen ui

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -1,6 +1,10 @@
 package com.plcoding.cryptotracker.crypto.presentation.models
 
+import android.icu.text.NumberFormat
 import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.util.Locale
 
 data class CoinUi(
     val id: String,
@@ -15,5 +19,29 @@ data class CoinUi(
 
 data class DisplayableNumber(
     val value: Double,
-    val formatted: Double
+    val formatted: String
 )
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercentHr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
